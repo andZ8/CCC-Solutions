3f5056a4f2ca668711bc8ed46ed462b59fa0f5b4
Andrew <162043915+andZ8@users.noreply.github.com>
2024-03-03 12:02:12 -0500

    Update 2024_S2.py

--- 2024_S2.py
+++ 2024_S2.py
@@ -1,4 +1,4 @@
-#15/15 Contest Day solution
+# 15/15 Contest Day solution
 n, length = input().split()
 n = int(n)
 length = int(length)
@@ -7,7 +7,7 @@ alternate = True
 prev = ""
 alpha = "abcdefghijklmnopqrstuvwxyz"
 
-#loop for each of the test cases
+# loop for each of the test cases
 for i in range(n):
   string = input()
   for i in alpha:
@@ -15,13 +15,13 @@ for i in range(n):
       #assign each letter a dictionary key to keep count
       dict[i] = string.count(i)
   for i in string:
-    #if i is the first letter (prev is blank) determine whether i is a "heavy" or "light" letter and set prev as either
+    # if i is the first letter (prev is blank) determine whether i is a "heavy" or "light" letter and set prev as either
     if prev == "":
       if dict[i] > 1:
         prev = "heavy"
       else:
         prev = "light"
-    #if prev exists already, determine if i follows the order
+    # if prev exists already, determine if i follows the order
     elif prev == "heavy":
       if dict[i] > 1:
         alternate = False
