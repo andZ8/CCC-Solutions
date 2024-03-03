dcdaddc1192aebc706ba727b4aef334e53779024
Andrew <162043915+andZ8@users.noreply.github.com>
2024-03-03 12:01:50 -0500

    Update 2024_S1.py

--- 2024_S1.py
+++ 2024_S1.py
@@ -1,9 +1,10 @@
+# Contest day 15/15
 n = int(input())
 count = 0
 people = []
 for i in range(n):
   people.append(int(input()))
-#check if people sitting opposite have the same number
+# check if people sitting opposite have the same number
 for i in range(n//2): #don't need to go all the way, halfway is enough
   if people[i] == people[i+(n//2)]:
     c += 2 
