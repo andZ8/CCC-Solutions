f054183f859872a75aedd1da92760d3546223fe1
Andrew <162043915+andZ8@users.noreply.github.com>
2024-03-03 11:50:51 -0500

    Create 2024_S1.py

---
+++ 2024_S1.py
@@ -0,0 +1,10 @@
+n = int(input())
+count = 0
+people = []
+for i in range(n):
+  people.append(int(input()))
+#check if people sitting opposite have the same number
+for i in range(n//2): #don't need to go all the way, halfway is enough
+  if people[i] == people[i+(n//2)]:
+    c += 2 
+print(c)
